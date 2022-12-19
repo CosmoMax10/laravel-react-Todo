5e5e5dd873e6dd60c5c72e0b30dd108072deab25
CosmoMax10 <cosmomax10@gmail.com>
2022-12-19 17:12:51 +0900

    DBのpostsデータをフロント側で取得しセット

--- src/resources/js/pages/Home.jsx
+++ src/resources/js/pages/Home.jsx
@@ -1,27 +1,54 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
 import { Button, Card } from '@material-ui/core';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
 import MainTable from '../components/MainTable';
 
-let rows = [
-    {
-        name: "モーリー",
-        content: "肩トレ",
-        editBtn: <Button color="secondary" variant="contained">編集</Button>,
-        deleteBtn: <Button color="primary" variant="contained">完了</Button>,
-    },{
-        name: "ドンキーコング",
-        content: "バナナ補給",
-        editBtn: <Button color="secondary" variant="contained">編集</Button>,
-        deleteBtn: <Button color="primary" variant="contained">完了</Button>,
-    },
-];
 
-
-//ヘッダーのコンテンツ用の配列定義
-const headerList = ['名前', 'タスク内容', '編集', '完了'];
 
 function Home() {
+
+    let rows = [
+        {
+            name: "モーリー",
+            content: "肩トレ",
+            editBtn: <Button color="secondary" variant="contained">編集</Button>,
+            deleteBtn: <Button color="primary" variant="contained">完了</Button>,
+        },{
+            name: "ドンキーコング",
+            content: "バナナ補給",
+            editBtn: <Button color="secondary" variant="contained">編集</Button>,
+            deleteBtn: <Button color="primary" variant="contained">完了</Button>,
+        },
+    ];
+
+
+    //ヘッダーのコンテンツ用の配列定義
+    const headerList = ['名前', 'タスク内容', '編集', '完了'];
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        getPostsData();
+    },[]);
+
+    const getPostsData = () => {
+
+        axios
+        .get('/api/posts')
+        .then(response => {
+        setPosts(response.data);
+        console.log(response.data);
+        })
+        .catch(() => {
+            console.log('通信に失敗しました。');
+        })
+
+    };
+
+
+
     return (
         <div>
             <h1>タスク管理</h1>
