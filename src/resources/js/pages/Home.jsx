ce833feef9ead01db608ae7426dee4aec49e1450
CosmoMax10 <cosmomax10@gmail.com>
2022-12-20 11:00:01 +0900

    新規登録のフロントエンド実装

--- src/resources/js/pages/Home.jsx
+++ src/resources/js/pages/Home.jsx
@@ -1,13 +1,31 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import PostFrom from '../components/PostFrom';
+import purple from '@material-ui/core/colors/purple';
 
 import { Button, Card } from '@material-ui/core';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
 import MainTable from '../components/MainTable';
 
+const useStyles = makeStyles((theme) => createStyles({
+    card: {
+        margin: theme.spacing(5),
+        padding: theme.spacing(3),
+    },
+    table: {
+        minWidth: 650,
+      },
+    tableHead: {
+        backgroundColor: purple['A100'],
+    },
+}));
+
 
 
 function Home() {
+
+
+    const classes = useStyles();
 
 
     //ヘッダーのコンテンツ用の配列定義
@@ -15,9 +33,12 @@ function Home() {
 
     const [posts, setPosts] = useState([]);
 
+    const [formData, setFormData] = useState({name: "", content: ""});
+
     useEffect(() => {
         getPostsData();
     },[]);
+
 
     const getPostsData = () => {
 
@@ -33,6 +54,39 @@ function Home() {
 
     };
 
+
+    const inputChange = (e) => {
+        const key = e.target.name;
+        const value = e.target.value;
+        formData[key] = value;
+        let data = Object.assign({}, formData);
+        setFormData(data);
+    }
+
+
+    const createPost = async() => {
+
+        if(formData == ""){
+            return;
+        }
+
+        await axios
+            .post('/api/post/create', {
+                name: formData.name,
+                content: formData.content
+            })
+            .then((res) => {
+                //戻り値をTodoにセット
+                const tempPosts = posts;
+                tempPosts.push(res.data);
+                setPosts(tempPosts);
+                setFormData("");
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
     let rows = [];
 
     posts.map((post) =>
@@ -47,7 +101,12 @@ function Home() {
     return (
         <div>
             <h1>タスク管理</h1>
-            <MainTable headerList={headerList} rows={rows} />
+            <Card className={classes.card}>
+                <PostFrom data={formData} btnFunc = {createPost} inputChange={inputChange}/>
+            </Card>
+            <Card className={classes.card}>
+                <MainTable headerList={headerList} rows={rows} />
+            </Card>
         </div>
     )
 }
